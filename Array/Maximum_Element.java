53023c28d6029786ffacdc3ec91b8a03e4d969a4
Ayush <ayushreso1234@gmail.com>
2023-07-29 21:08:55 +0530

    29th july

--- Array/Maximum_Element.java
+++ Array/Maximum_Element.java
@@ -2,5 +2,21 @@
 public class Maximum_Element {
     public static void main(String[] args) {
 
+        int arr[] = {12, 32, 43, 64, 2, 54, 32, 87, 5, 43};
+
+        findMax(arr);// method call
+
+    }
+
+    //method declaration
+    public static void findMax(int arr[]) {
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {  //for loop for finding maxium
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("maximum element is " + max);
     }
 }
